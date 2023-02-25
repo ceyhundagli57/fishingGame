92d200f6161c89b7d69a8e59097c8ed8a85e696b
unknown <ceyhundagli57@gmail.com>
2023-02-24 19:24:08 -0800

    initial commit

---
+++ source/repos/Fishing/Fishing/Bluefin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Fishing
+{
+    public class Bluefin : Fish
+    {
+        public Bluefin(int probability) : base(probability)
+        {
+            MarketPrice = 500;
+            Name = "bluefin";
+        }
+
+    }
+}
---
+++ source/repos/Fishing/Fishing/Catfish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fishing
+{
+    public class Catfish : Fish
+    {
+        public Catfish(int probability) : base(probability)
+        {
+            MarketPrice = 10;
+            Name = "catfish";
+        }
+    }
+}
